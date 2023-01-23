e3c6fbe50b6c6df8a87ecf1bbc46fa47632457f0
DSYCRAFT <natyloren26@hotmail.com>
2023-01-23 16:53:51 -0500

    Add the road

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
+# Road parameters
+ROAD_W = 500
+ROADMARK_W = 10
+
 # Set windows size
 screen = pygame.display.set_mode( SIZE )
 
@@ -19,6 +23,18 @@ screen.fill((49, 185, 52))
 # Icon
 ICON = pygame.image.load("sprites/icon.png")
 pygame.display.set_icon(ICON)
+
+# Draw the road
+pygame.draw.rect(screen, (31, 31, 31), (SCREEN_WIDTH/2 - ROAD_W/2, 0, ROAD_W, SCREEN_HEIGHT))
+
+# Draw the roadmark
+pygame.draw.rect(screen, (255, 240, 60), (SCREEN_WIDTH/2 - ROADMARK_W/2, 0, ROADMARK_W, SCREEN_HEIGHT))
+
+# Draw the white lines
+pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH/2 - ROAD_W/2 + ROADMARK_W * 2, 0, ROADMARK_W, SCREEN_HEIGHT))
+
+# Draw the white lines 2
+pygame.draw.rect(screen, (255, 255, 255), (SCREEN_WIDTH/2 + ROAD_W/2 - ROADMARK_W * 3, 0, ROADMARK_W, SCREEN_HEIGHT))
 
 # Update changes
 pygame.display.update()
